3879de0f09f7afa92a10515f0b65728d24bfd49e
alumno_fp <jesus.perez@escuelaestech.es>
2018-10-25 18:10:02 +0200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Pedimos por consola el número.
+        System.out.println("Introduzca el número a comprobar:\n");
+        Scanner sc=new Scanner(System.in);
+        double numero=sc.nextDouble();
+       //If para comprobar si el número es mayor o menor que 0
+        if (numero>=0){
+            System.out.printf("El número es positivo.");
+        }else{
+            System.out.printf("El número es negativo.");
+        }
+    }
+}
